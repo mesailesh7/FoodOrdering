d97204fa68137e280700736d22f9d189e845262d
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2024-05-03 12:54:48 -0600

    Update CartListItem.tsx

--- src/components/CartListItem.tsx
+++ src/components/CartListItem.tsx
@@ -17,7 +17,7 @@ const CartListItem = ({ cartItem }: CartListItemProps) => {
   return (
     <View style={styles.container}>
       <Image
-        source={{ uri: cartItem.product.image || defaultPizzaImagemage }}
+        source={{ uri: cartItem.product.image || defaultPizzaImage }}
         style={styles.image}
         resizeMode="contain"
       />
